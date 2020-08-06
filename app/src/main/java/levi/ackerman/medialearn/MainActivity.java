e7c06c5f31ce2a88008c8795664619a64a6714ef
zhengxian.lzx <zhengxian.lzx@alibaba-inc.com>
2020-08-06 14:01:15 +0800

    [feat] 连接libTest.so动态库成功

--- app/src/main/java/levi/ackerman/medialearn/MainActivity.java
+++ app/src/main/java/levi/ackerman/medialearn/MainActivity.java
@@ -1,5 +1,6 @@
 package levi.ackerman.medialearn;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void testSo(View view) {
+
     }
 }
---
+++ app/src/main/jni/native.c
@@ -0,0 +1,10 @@
+//
+// Created by ali on 2020/8/6.
+//
+
+#include <stdio.h>
+
+void hello(){
+    printf("hello");
+}
+
