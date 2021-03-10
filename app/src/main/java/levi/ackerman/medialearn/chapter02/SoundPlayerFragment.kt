2b793e33343376b8d040c66c0fd463098e8a695e
lizhengxian.ackerman <lizhengxian.ackerman@bytedance.com>
2021-03-10 10:58:51 +0800

    AudioTrack新API初始化音频成功

--- app/src/main/java/levi/ackerman/medialearn/chapter02/SoundPlayerFragment.kt
+++ app/src/main/java/levi/ackerman/medialearn/chapter02/SoundPlayerFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import levi.ackerman.medialearn.BaseFragment
 import levi.ackerman.medialearn.R
 import java.io.File
@@ -39,7 +40,7 @@ class SoundPlayerFragment : BaseFragment() {
         super.initData()
         btnPlayAudioTrack = rootView.findViewById<Button>(R.id.btn_play_audio_track)
         btnPlayOpenSL = rootView.findViewById<Button>(R.id.btn_play_audio_opensl)
-        btnPlayAudioTrack.setOnClickListener{
+        btnPlayAudioTrack.setOnClickListener {
             playWithAudioTrack()
         }
     }
@@ -48,44 +49,57 @@ class SoundPlayerFragment : BaseFragment() {
     }
 
     private fun playWithAudioTrack() {
-        val audioAttributes = AudioAttributes.Builder().build()
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
         val sampleRate = 44100
-        val format =
-            AudioFormat.Builder().setSampleRate(sampleRate).setEncoding(AudioFormat.ENCODING_PCM_16BIT).build()
+        val format = AudioFormat.Builder()
+            .setSampleRate(sampleRate)
+            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+            .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+            .build()
         val bufferSize =
             AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT)
-//        val audioTrack = AudioTrack(
-//            audioAttributes,
-//            format,
-//            bufferSize,
-//            AudioTrack.MODE_STREAM,
-//            AudioManager.AUDIO_SESSION_ID_GENERATE
-//        )
-
         val audioTrack = AudioTrack(
-            AudioManager.STREAM_MUSIC,
-            sampleRate,
-            AudioFormat.CHANNEL_OUT_STEREO,
-            AudioFormat.ENCODING_PCM_16BIT,
+            audioAttributes,
+            format,
             bufferSize,
-            AudioTrack.MODE_STREAM
+            AudioTrack.MODE_STREAM,
+            AudioManager.AUDIO_SESSION_ID_GENERATE
         )
 
+//        val audioTrack = AudioTrack(
+//            AudioManager.STREAM_MUSIC,
+//            sampleRate,
+//            AudioFormat.CHANNEL_OUT_STEREO,
+//            AudioFormat.ENCODING_PCM_16BIT,
+//            bufferSize,
+//            AudioTrack.MODE_STREAM
+//        )
+
+        if (audioTrack.state == AudioTrack.STATE_UNINITIALIZED){
+            Toast.makeText(activity!!, "AudioTrack初始化失败!",Toast.LENGTH_SHORT).show()
+            return
+        }
+
         audioTrack.play()
-        object :Thread(){
+        object : Thread() {
             override fun run() {
                 super.run()
                 val fis = activity!!.assets.open("yura.pcm")
                 val buf = ByteArray(128)
                 var size = 0
-                while(true){
+                while (true) {
                     size = fis.read(buf)
-                    if (size <= 0){
+                    if (size <= 0) {
                         break
                     }
-                    audioTrack.write(buf,0,size)
+                    audioTrack.write(buf, 0, size)
                 }
                 fis.close()
+                audioTrack.stop()
+                audioTrack.release()
             }
         }.start()
     }
